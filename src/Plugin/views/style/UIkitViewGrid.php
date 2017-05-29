0f823c4458c7b1f1a5178797eb107f4e6633ab74
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-05-29 13:29:19 -0400

    UIkit grid views plugin not updating options

--- src/Plugin/views/style/UIkitViewGrid.php
+++ src/Plugin/views/style/UIkitViewGrid.php
@@ -20,45 +20,16 @@ use Drupal\Component\Utility\Html;
  * )
  */
 class UIkitViewGrid extends StylePluginBase {
+
   /**
-   * Overrides \Drupal\views\Plugin\views\style\StylePluginBase::usesRowPlugin.
-   *
-   * @var bool
+   * {@inheritdoc}
    */
   protected $usesRowPlugin = TRUE;
 
   /**
-   * Overrides \Drupal\views\Plugin\views\style\StylePluginBase::usesRowClass.
-   *
-   * @var bool
+   * {@inheritdoc}
    */
   protected $usesRowClass = TRUE;
-
-  /**
-   * Return the token-replaced row or column classes for the specified result.
-   *
-   * @param int $result_index
-   *   The delta of the result item to get custom classes for.
-   * @param string $type
-   *   The type of custom grid class to return, either "row" or "col".
-   *
-   * @return string
-   *   A space-delimited string of classes.
-   */
-  public function getCustomClass($result_index, $type) {
-    if (isset($this->options[$type . '_class_custom'])) {
-      $class = $this->options[$type . '_class_custom'];
-      if ($this->usesFields() && $this->view->field) {
-        $class = strip_tags($this->tokenizeValue($class, $result_index));
-      }
-
-      $classes = explode(' ', $class);
-      foreach ($classes as &$class) {
-        $class = Html::cleanCssIdentifier($class);
-      }
-      return implode(' ', $classes);
-    }
-  }
 
   /**
    * {@inheritdoc}
@@ -66,21 +37,13 @@ class UIkitViewGrid extends StylePluginBase {
   protected function defineOptions() {
     $options = parent::defineOptions();
 
-    // Default grid view options.
-    $options['columns'] = ['default' => '4'];
-    $options['automatic_width'] = ['default' => TRUE];
-    $options['alignment'] = ['default' => 'horizontal'];
-    $options['col_class_custom'] = ['default' => ''];
-    $options['col_class_default'] = ['default' => TRUE];
-    $options['row_class_custom'] = ['default' => ''];
-    $options['row_class_default'] = ['default' => TRUE];
-
     // UIkit grid view options.
     $options['width_small'] = ['default' => 'uk-grid-width-small-1-1'];
     $options['width_medium'] = ['default' => 'uk-grid-width-medium-1-2'];
     $options['width_large'] = ['default' => 'uk-grid-width-large-1-3'];
     $options['width_xlarge'] = ['default' => 'uk-grid-width-xlarge-1-4'];
     $options['grid_gutter'] = ['default' => 'default'];
+
     return $options;
   }
 
@@ -119,7 +82,7 @@ class UIkitViewGrid extends StylePluginBase {
         '#type' => 'select',
         '#title' => $this->t("uk-grid-width-${size}-*"),
         '#required' => TRUE,
-        '#default_value' => isset($this->options["width_${size}"]) ? $this->options["width_${size}"] : NULL,
+        '#default_value' => $this->options['column_widths']['container']["width_${size}"],
         '#options' => [
           "uk-grid-width-${size}-1-1" => 1,
           "uk-grid-width-${size}-1-2" => 2,
@@ -137,7 +100,7 @@ class UIkitViewGrid extends StylePluginBase {
       '#type' => 'select',
       '#title' => $this->t('Grid gutter'),
       '#required' => TRUE,
-      '#default_value' => isset($this->options['grid_gutter']) ? $this->options['grid_gutter'] : NULL,
+      '#default_value' => $this->options['grid_gutter'],
       '#options' => [
         'default' => $this->t('Default gutter'),
         'uk-grid-small' => $this->t('Small gutter'),
@@ -147,6 +110,24 @@ class UIkitViewGrid extends StylePluginBase {
       ],
       '#description' => $this->t('<p>Grids automatically create a horizontal gutter between columns and a vertical one between two succeeding grids. By default, the grid gutter is wider on large screens.<br /><strong>Note</strong>: <em class="placeholder">Grid collapse</em> removes the grid gutter.</p>'),
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCustomClass($result_index, $type) {
+    if (isset($this->options[$type . '_class_custom'])) {
+      $class = $this->options[$type . '_class_custom'];
+      if ($this->usesFields() && $this->view->field) {
+        $class = strip_tags($this->tokenizeValue($class, $result_index));
+      }
+
+      $classes = explode(' ', $class);
+      foreach ($classes as &$class) {
+        $class = Html::cleanCssIdentifier($class);
+      }
+      return implode(' ', $classes);
+    }
   }
 
 }
